e35ace5485c4cc81a6aee16eb1aa17dac1fc3ac9
rcorob <79815272+rcorob@users.noreply.github.com>
2023-09-13 13:28:07 -0500

    Added fileIO, floorTile files. (#19)
    * Added fileIO, floorTile files.
    
    Not sure if I'm adding my changes correctly, but I'm adding the files for File I/O and map structure, and the Floor Tile class.
    
    * formatted files using black

---
+++ fileIO.py
@@ -0,0 +1,22 @@
+#
+#   fileIO.py
+#   Author: Ryan Corob (rac0026@uah.edu)
+#   Date Created: 10 September 2023
+#   This file contains the structure for importing/exporting map layouts.
+#
+
+import floorTile
+import json
+
+map = dict()
+for x in range(0, 2000):
+    for y in range(0, 800):
+        map[(x, y)] = floorTile
+
+
+def exportFloorPlan(floorPlan, fileName):
+    with open(fileName, "w") as fid:
+        json.dump(floorPlan, fid)
+
+
+exportFloorPlan(map, "testMap.json")
---
+++ floorTile.py
@@ -0,0 +1,19 @@
+#
+#   floorTile.py
+#   Author: Ryan Corob (rac0026@uah.edu)
+#   Date Created: 08 September 2023
+#   This file contains the class for the map's Floor Tiles.
+#   These objects will be held my a Map dictionary, which holds
+#       an x and y coordinate alongside this class
+#
+
+
+class floorTile:
+    def __init__(self):
+        self.dirtiness = 0.0
+        self.carpetType = 0
+        self.parentRoomName = "Default Room"
+
+    def clean(self, efficiency):
+        # TO BE IMPLEMENTED
+        1
