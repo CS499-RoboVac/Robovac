4e89fcc05c25a0795f80d84f690f0604370bbf9c
Brenneman-SBO <brennemansbo@gmail.com>
2023-10-01 16:38:46 -0500

    Squashed commit of the following:
    commit 5d66a3125633e323b013802aedd3e591df79c772
    Author: Brenneman-SBO <brennemansbo@gmail.com>
    Date:   Sun Oct 1 16:36:58 2023 -0500
    
        Ran through Black
    
    commit d1752efdc2d29967f1d8dcbf31d3f1505f9d1ce6
    Author: Brenneman-SBO <brennemansbo@gmail.com>
    Date:   Sun Oct 1 16:02:30 2023 -0500
    
        clarified variable names
    
    commit 7fcf3ffea6e3121fb2eab95be18d22f1e7b14d74
    Author: Brenneman-SBO <brennemansbo@gmail.com>
    Date:   Mon Sep 25 16:47:51 2023 -0500
    
        (removed test material)
    
    commit 88fdd6cbbc4840b9f7f71349b6e73e4f31d45fca
    Author: Brenneman-SBO <brennemansbo@gmail.com>
    Date:   Mon Sep 25 16:46:29 2023 -0500
    
        added map creation, saving, loading
    
    commit f3ad7373fffc9d7a80e7b17f61e84d229ff611e1
    Author: Brenneman-SBO <brennemansbo@gmail.com>
    Date:   Thu Sep 14 18:45:19 2023 -0500
    
        add real collision
    
        no longer a stub :)
    
    commit 246c89fc3e011644e3bbe4278c527ab595f87021
    Author: Brenneman-SBO <142832683+Brenneman-SBO@users.noreply.github.com>
    Date:   Wed Sep 13 13:49:37 2023 -0500
    
        Add files via upload
    
        vec2 class for easier ops on 2d vectors, and (unfinished!) collision checker logic using our dictionary format

---
+++ UsefulFuncs.py
@@ -0,0 +1,68 @@
+class Vec2:
+    def __init__(self, x, y=None):
+        if isinstance(x, (tuple, list, Vec2)):
+            if len(x) != 2:
+                raise ValueError("Input tuple or list must have exactly 2 elements")
+            self.x, self.y = x
+        else:
+            if y == None:
+                raise ValueError(
+                    "Vec2() takes 2 values or 1 list/tuple but only 1 was value was given"
+                )
+            self.x = x
+            self.y = y
+
+    # funky way of making the operator stuff smaller
+    def selector(self, b, op):
+        return (
+            Vec2(op(self.x, b.x), op(self.y, b.y))
+            if type(b) == Vec2
+            else Vec2(op(self.x, b), op(self.y, b))
+        )
+
+    def __add__(self, b):
+        return self.selector(b, lambda x, y: x + y)
+
+    def __sub__(self, b):
+        return self.selector(b, lambda x, y: x - y)
+
+    def __mul__(self, b):
+        return self.selector(b, lambda x, y: x * y)
+
+    def __truediv__(self, b):
+        return self.selector(b, lambda x, y: x / y)
+
+    def __eq__(self, b):
+        return type(b) == Vec2 and self.x == b.x and self.y == b.y
+
+    # Allows for list like indexing, example Vec2(2,3)[0], returns 2
+    def __getitem__(self, index):
+        return self.y if index else self.x
+
+    def __str__(self):
+        return "[" + str(self.x) + "," + str(self.y) + "]"
+
+    def __len__(self):
+        return 2
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def length(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def unit(self):
+        return self / math.sqrt(self.x**2 + self.y**2)
+
+
+def Collision(
+    pos, r, m
+):  # (float, float) pos, float r, {(int, int):FloorTile} m -> bool
+    for x in range(floor(pos.x - r), ceil(pos.x + r) + 1):
+        for y in range(floor(pos.y - r), ceil(pos.y + r) + 1):
+            if (
+                Vec2(x, y) - pos
+            ).length() < r:  # we looped over a square; now we're using the ones within a radius in that square
+                if not (x, y) in m:
+                    return True  # there was a collision
+    return False  # no collisions, we can go onwards
