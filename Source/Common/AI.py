ddfdf22ff28ab9678bfb1333ce27d07722403371
Brenneman-SBO <brennemansbo@gmail.com>
2023-10-16 16:48:54 -0500

    black

--- Source/Common/AI.py
+++ Source/Common/AI.py
@@ -1,7 +1,9 @@
 import random
+
+
 class BiasedRandomAI:
     def __init__(self):
         return
 
-    def update(self, isColliding:bool):
+    def update(self, isColliding: bool):
         return (random.uniform(-0.5, 1), random.uniform(-1, 1))
