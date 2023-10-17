f8fe74da36fde69136b76f86049116f5876263e9
Brenneman-SBO <brennemansbo@gmail.com>
2023-10-17 16:50:52 -0500

    removed minor junk code

--- Source/Common/AI.py
+++ Source/Common/AI.py
@@ -2,8 +2,5 @@ import random
 
 
 class BiasedRandomAI:
-    def __init__(self):
-        return
-
     def update(self, isColliding: bool):
         return (random.uniform(-0.5, 1), random.uniform(-1, 1))
