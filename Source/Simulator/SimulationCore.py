a13e939b5fdfed14f8a3452d465e3d791f4e92c7
John Ingram <jsi0004@uah.edu>
2023-10-22 20:16:59 -0500

    Fixed variable names in SimulationCore

--- Source/Simulator/SimulationCore.py
+++ Source/Simulator/SimulationCore.py
@@ -9,30 +9,30 @@ class Simulation:
         ai: any object with a method that matches the update(bool) -> (float, float) signature. Outputs should both be in the range [-1, 1].
         robot: the Robot object
         """
-        self.AI = ai
-        self.FloorPlan = floor
+        self.ai = ai
+        self.floor_plan = floor
         if robot.validate() and not Collision(robot.pos, robot.diameter / 2, floor):
             self.robot = robot
         else:
             raise ValueError("Robot not valid, or collides with terrain")
 
-        self.ElapsedTime = 0
-        self.IsColliding = False
-        self.SimSpeed = 1
-        self.DefaultHertz = 60
+        self.elapsed_time = 0
+        self.is_colliding = False
+        self.sim_speed = 1
+        self.default_tickrate = 60
 
     def update(self, dT):
         """dT, in seconds, is the real time since the last Update call"""
-        self.robot.doCleaning(self.FloorPlan, dT)
+        self.robot.doCleaning(self.floor_plan, dT)
         # these values are in the [-1, 1] range, representing fraction of the robot's maximum ability
-        speed, dTheta = self.AI.update(self.IsColliding)
-        self.robot.facing += dTheta * self.robot.maxTurn * dT * self.SimSpeed
+        speed, dTheta = self.ai.update(self.is_colliding)
+        self.robot.facing += dTheta * self.robot.maxTurn * dT * self.sim_speed
         ProspectivePosition = (
-            self.robot.pos + speed * self.robot.maxSpeed * dT * self.SimSpeed
+            self.robot.pos + speed * self.robot.maxSpeed * dT * self.sim_speed
         )
-        self.IsColliding = Collision(
-            ProspectivePosition, self.robot.diameter / 2, self.FloorPlan
+        self.is_colliding = Collision(
+            ProspectivePosition, self.robot.diameter / 2, self.floor_plan
         )
-        if not self.IsColliding:
+        if not self.is_colliding:
             # the robot's motion is valid
             self.robot.pos = ProspectivePosition
