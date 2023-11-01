725e12db0e891af33702fe61f6a22f1256cb6068
Brenneman-SBO <142832683+Brenneman-SBO@users.noreply.github.com>
2023-11-01 13:50:26 -0500

    Sam ai and floors (#65)
    * AI done, Sim in progress
    
    * more Simulation updates
    
    * ran black
    
    ---------
    
    Co-authored-by: John Ingram <j123j7@gmail.com>

---
+++ Source/Common/Primitives.py
@@ -0,0 +1,70 @@
+from Common.Util import Vec2
+import math
+
+
+class Rectangle:
+    def __init__(self, minCorner: Vec2, maxCorner: Vec2, exclusion: bool):
+        self.minCorner = minCorner
+        self.maxCorner = maxCorner
+        self.exclusion = exclusion
+
+    def BoundingBox(self):
+        return (self.minCorner, self.maxCorner)
+
+    def isValid(self, point: Vec2):
+        """Checks to see if point is inside of the rectangle, returns inverted result if the rectangle is an exclusion primitive
+        point: Vec2
+        returns: boolean
+        """
+        inside = (
+            point.x > self.minCorner.x
+            and point.y > self.minCorner.y
+            and point.x < self.maxCorner.x
+            and point.y < self.maxCorner.y
+        )
+        return inside ^ self.exclusion  # ^ is XOR (inverts result if exclusion is true)
+
+
+class Circle:
+    def __init__(self, center: Vec2, radius: float, exclusion: bool):
+        self.center = center
+        self.radius = radius
+        self.exclusion = exclusion
+
+    def BoundingBox(self):
+        # returns vec2s representing the top left and bottom right corners of the bounds of the shape
+        diag = Vec2(self.radius, self.radius)
+        return (self.center - diag, self.center + diag)
+
+    def isValid(self, point: Vec2):
+        """Checks to see if point is inside of the circle, returns inverted result if the circle is an exclusion primitive
+        point: Vec2
+        returns: boolean
+        """
+        inside = (self.center - point).length() < self.radius
+        return inside ^ self.exclusion  # ^ is XOR (inverts result if exclusion is true)
+
+
+def PrimitiveInclusion(shapes, point):
+    """function that inputs a list of primitive shapes and a point and returns true if the point in a valid zone for all primitives in the list
+    point: Vec2
+    shapes: list<shapes>
+    return: boolean"""
+    return all([primitive.isValid(point) for primitive in shapes])
+
+
+# TODO: make this work with the new robot class, also this will probably need to be changed and or moved to the floorplan class once that exists
+def Collision(pos: Vec2, r: float, FloorPlan: dict):
+    """function that takes a position and radius, and a floorplan dict, returns True if a collision happens or False if the position is clear
+    pos : Vec2 of the vacuum's position,
+    r : float radius of the vacuum,
+    FloorPlan : dict with entries of the form (int, int): FloorTile"""
+
+    for x in range(math.floor(pos.x - r), math.ceil(pos.x + r) + 1):
+        for y in range(math.floor(pos.y - r), math.ceil(pos.y + r) + 1):
+            if (
+                Vec2(x, y) - pos
+            ).length() < r:  # we looped over a square; now we're using the ones within a radius in that square
+                if not (x, y) in FloorPlan:
+                    return True  # there was a collision
+    return False  # no collisions, we can go onwards
--- Source/Simulation/OpenSimulation.py
+++ Source/Simulation/OpenSimulation.py
@@ -1,18 +1,63 @@
 from PyQt5 import QtCore, QtGui, QtWidgets, QtTest
+from PyQt5.QtCore import QObject, QThread, pyqtSignal, QRectF
+from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtGui import QPainter, QColor, QBrush
 from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
+    QGraphicsScene,
     QPushButton,
     QLabel,
     QVBoxLayout,
     QWidget,
+    QGraphicsItem,
+    QMessageBox,
+    QFileDialog,
 )
+
 import sys
+import time
+import json
+import os
+import Common.Primitives as Primitives
+from Common.Util import Vec2
+import Common.Colors as Colors
+import Simulation.AI as AI
+import numpy as np
+
+# import Simulator.SimulationCore as SimulationCore
+
 from Views.ui_sim import Ui_SimWindow
 
 # from FloorPlanDesigner.openFPD import fpdWindowApp
 import FloorPlanDesigner.openFPD as OpenFPD
 import IntroWindow.openIntro as OpenIntro
+
+
+class RectangleItem(QGraphicsItem):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.rect = QRectF(x, y, width, height)
+
+    def boundingRect(self):
+        return self.rect
+
+    def paint(self, painter, option, widget):
+        painter.setBrush(QColor(200, 0, 0))  # Set the fill color
+        painter.drawRect(self.rect)
+
+
+class CircleItem(QGraphicsItem):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.rect = QRectF(x, y, width, height)
+
+    def boundingRect(self):
+        return self.rect
+
+    def paint(self, painter, option, widget):
+        painter.setBrush(QColor(200, 0, 0))  # Set the fill color
+        painter.drawRect(self.rect)
 
 
 class simWindowApp(QMainWindow, Ui_SimWindow):
@@ -23,6 +68,41 @@ class simWindowApp(QMainWindow, Ui_SimWindow):
         self.fpds = []
         self.oprs = []
         self.main = []
+        self.shapes = []
+        # MAKE SPEED VALUE HERE TODO
+        self.SimSpeed = 1
+        self.Robot = None
+        self.dirt = None
+        self.floorplansDir = (
+            os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+            + "/Floor Plans/"
+        )
+
+        self.graphicsView.scene = QGraphicsScene()
+        self.graphicsView.setScene(self.graphicsView.scene)
+
+    # Create a worker class to run the simulation in a seperate thread
+    class Worker(QObject):
+        def __init__(self, parent):
+            self.parent = parent
+
+        finished = pyqtSignal()
+
+        def run(self):
+            """Simulation thread/run loop"""
+            # Use the currently loaded floor plan?
+            tl, br = self.BoundingBox()
+            self.parent.robot = self.parent.InstanceRobot()
+            self.parent.dirt = np.zeros(
+                (math.ceil(abs(tl.x - br.x)), math.ceil(abs(tl.y - br.y)))
+            )
+            # Sim = Simulation(self.parent.shapes, self.parent.InsanceAI(), self.parent.InstanceRobot())
+            while False:
+                # Read the simulation rate to control the run loop
+                # dT = self.parent.SimSpeed
+                Sim.update()
+                pass
+            self.finished.emit()
 
     def openFPD(self):
         self.fpds.append(OpenFPD.fpdWindowApp())
@@ -32,11 +112,95 @@ class simWindowApp(QMainWindow, Ui_SimWindow):
         self.main.append(OpenIntro.mainWindowApp())
         self.main[-1].show()
 
+    def beginSimulation(self):
+        """Simulation initialization logic"""
+        # Check to see if all values are valid for starting the simulation:
+        if len(self.shapes):
+            # Create a QThread object
+            self.thread = QThread()
+            # Create a worker object
+            self.worker = self.Worker(self)
+            # Move worker to the thread
+            self.worker.moveToThread(self.thread)
+            # Connect signals and slots
+            self.thread.started.connect(self.worker.run)
+            self.worker.finished.connect(self.thread.quit)
+            self.worker.finished.connect(self.worker.deleteLater)
+            self.thread.finished.connect(self.thread.deleteLater)
+
+            # Start the thread
+            self.thread.start()
+
+            self.SimulationButton.setEnabled(False)
+            self.thread.finished.connect(lambda: self.SimulationButton.setEnabled(True))
+            self.thread.finished.connect(
+                lambda: self.SimulationButton.setText("Finished Simulation")
+            )
+        else:
+            # Emit failure message because a condition was not met
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Simulation Parameter Invalid or Floor Plan not loaded")
+            msg.setInformativeText("TMP, replace with informative information")
+            msg.setWindowTitle("Error")
+            msg.exec_()
+            pass
+
+    def InstanceRobot(self):
+        # return Robot() #TODO read sliders
+        pass
+
+    def BoundingBox(self):
+        """returns bounds of the whole floorplan, as top-left and bottom-right Vec2s"""
+        left = min(self.shapes, key=lambda s: s.BoundingBox()[0].x)
+        top = min(self.shapes, key=lambda s: s.BoundingBox()[0].y)
+        right = max(self.shapes, key=lambda s: s.BoundingBox()[1].x)
+        bottom = max(self.shapes, key=lambda s: s.BoundingBox()[1].y)
+        return (Vec2(left, top), Vec2(right, bottom))
+
+    def loadFloorPlan(self):
+        """
+        Loads a floorplan from a file selected by the user using a file dialog.
+        The floorplan is stored in a JSON file format.
+        """
+        opts = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(
+            self,
+            "Floorplan Designer - Load Floorplan",
+            self.floorplansDir,
+            "Floor Plan Designer Files (*.fpd)",
+            options=opts,
+        )
+        if fileName:
+            self.graphicsView.scene.clear()
+            self.shapes = []
+            with open(fileName, "r") as inFile:
+                fp = json.load(inFile)
+
+            for key in fp.keys():
+                # The fact that there is a hard coded dimension like this hurts my soul
+                x = int(int(fp[key]["x1"]) / 182)
+                y = int(int(fp[key]["y1"]) / 182)
+                w = int(int(fp[key]["width"]) / 182)
+                h = int(int(fp[key]["height"]) / 182)
+                furniture = fp[key]["furniture"]  # What even is this parameter???
+                # EVERYTHING IS A RECTANGLE YAY\s TODO
+                # EVERYTHING IS AN INCLUSION YAY (eventually there will need to be a conditional on the isExclusion variable)
+                isExclusion = False  # TODO
+                self.shapes.append(
+                    Primitives.Rectangle(Vec2(x, y), Vec2(x + w, y + h), isExclusion)
+                )
+                # TODO deal with not rectangles for rendering
+                # This renders the rectangle to the screen
+                rect = RectangleItem(x, y, w, h)  # parameters are x, y, width, height
+                # Add the rectangle to the scene
+                self.graphicsView.scene.addItem(rect)
+
     def connectButtons(self):
         self.BacktoMainButton.clicked.connect(self.openMain)
         self.EditFloorPlanButton.clicked.connect(self.openFPD)
-        # self.SimulationButton.clicked.connect()
-        # self.LoadFloorPlanButton.clicked.connect()
+        self.SimulationButton.clicked.connect(self.beginSimulation)
+        self.LoadFloorPlanButton.clicked.connect(self.loadFloorPlan)
         # self.SimSpeedButton.clicked.connect()
         # self.VacuumLoadButton.clicked.connect()
         # self.VacuumSaveButton.clicked.connect()
